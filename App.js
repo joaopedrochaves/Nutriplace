f1e7084bbf8e4ae9998bc38c6204ba3b08b59425
joaopedrochaves <88900854+joaopedrochaves@users.noreply.github.com>
2024-10-13 14:29:54 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ProductsScreen from './screens/ProductsScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Products" component={ProductsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
